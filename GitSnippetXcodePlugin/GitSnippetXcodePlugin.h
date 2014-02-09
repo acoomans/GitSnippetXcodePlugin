69547484ff3a6b2a2b06ed72b37373cafd154115
Arnaud Coomans <arnaud.coomans@gmail.com>
2014-02-09 15:03:18 -0800

    Initial commit

---
+++ GitSnippetXcodePlugin/GitSnippetXcodePlugin.h
@@ -0,0 +1,13 @@
+//
+//  GitSnippetXcodePlugin.h
+//  GitSnippetXcodePlugin
+//
+//  Created by Arnaud Coomans on 09/02/14.
+//  Copyright (c) 2014 Arnaud Coomans. All rights reserved.
+//
+
+#import <AppKit/AppKit.h>
+
+@interface GitSnippetXcodePlugin : NSObject
+
+@end
